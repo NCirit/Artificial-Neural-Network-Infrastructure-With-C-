7fb9165ac5d024edda3d17cac5e5dc2e88aac2a5
NCirit <44056802+NCirit@users.noreply.github.com>
2019-04-22 17:24:23 +0300

    Add files via upload

---
+++ ANN.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Matrix;
+namespace Matrix
+{
+    class ANN
+    {
+        public int inputCount;
+        public int outputCount;
+        public int hLayerCount;
+        public int[] neuronCount;
+
+        public float[] bias;
+        public float learningRate;
+
+        public Matrix inputs;
+        public Matrix targets;
+        public Matrix outputs;
+
+
+        public Matrix[] hiddenLayers;
+        public Matrix[] weights;
+
+        Random rnd; // for selecting a random tranning example in training set
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputs">it contains row by row each training-set elements</param>
+        /// <param name="targets">it contains row by row each actual value of inputs</param>
+        /// <param name="outputCount">it specify neuron count of outpu layer</param>
+        /// <param name="hLayerCount">it specify hidden layer count</param>
+        /// <param name="hLayerNeuronCount">it specify neuron count for each hidden layer</param>
+        public ANN(float[,] inputs, float[,] targets, int outputCount, int hLayerCount, int[] hLayerNeuronCount)
+        {
+
+            this.inputCount = inputs.GetLength(1);
+            this.outputCount = outputCount;
+            this.inputs = new Matrix(inputs);
+            this.targets = new Matrix(targets);
+            this.hLayerCount = hLayerCount;
+            this.neuronCount = hLayerNeuronCount;
+            init();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputs">it contains row by row each training-set elements</param>
+        /// <param name="targets">it contains row by row each actual value of inputs</param>
+        /// <param name="outputCount">it specify neuron count of outpu layer</param>
+        /// <param name="hLayerCount">it specify hidden layer count</param>
+        /// <param name="hLayerNeuronCount">it specify neuron count for each hidden layer</param>
+        public ANN(Matrix inputs, Matrix targets, int outputCount, int hLayerCount, int[] hLayerNeuronCount)
+        {
+            this.inputs = inputs;
+            this.inputCount = inputs.column;
+            this.targets = targets.GetTranspose();
+            this.hLayerCount = hLayerCount;
+            this.neuronCount = hLayerNeuronCount;
+            this.outputCount = outputCount;
+
+            init();
+        }
+        /// <summary>
+        /// This method initialize the bias , hidden layers and weights.
+        /// The variable weights is a multi-dimensional Matrix, that contain weight between layers. w[0] is between input layer and if exist hidden layer else outputlayer.
+        /// The weights[i] is weight between hidden layer [i-1] and hidden layer [i], each column of weights[i] does specify weight between inputs and a hidden layer neuron .
+        /// </summary>
+        private void init()
+        {
+            rnd = new Random();
+            this.bias = new float[this.hLayerCount+1];
+            hiddenLayers = new Matrix[this.hLayerCount];
+            for (int i = 0; i < this.hLayerCount; i++)
+            {
+                hiddenLayers[i] = new Matrix(1, this.neuronCount[i]);
+            }
+            weights = new Matrix[this.hLayerCount + 1];
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if (this.hLayerCount != 0)
+                        weights[i] = new Matrix(this.inputCount, this.hiddenLayers[i].column);
+                    else
+                    {
+                        weights[i] = new Matrix(this.inputCount, this.outputCount);
+                    }
+                }
+                else if (i == weights.Length - 1)
+                {
+                    weights[i] = new Matrix(this.hiddenLayers[i - 1].column, this.outputCount);
+                }
+                else
+                {
+                    weights[i] = new Matrix(this.hiddenLayers[i - 1].column, this.hiddenLayers[i].column);
+                }
+
+            }
+
+            FillWeights();
+        }
+
+        /// <summary>
+        /// Filling weights with random numbers between -1 and 1.
+        /// </summary>
+        public void FillWeights()
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i].RandomFill();
+            }
+        }
+
+
+
+        public void Train(int iterationCount)
+        {
+            for (int i = 0; i < iterationCount; i++)
+            {
+                CalculateH();
+            }
+            Console.WriteLine("Weights:");
+            this.PrintWeights();
+            Console.WriteLine("Outputs:");
+            this.outputs.PrintMatrix();
+        }
+
+        /// <summary>
+        /// Calculating for neurons of hidden layers and Backpropagation
+        /// </summary>
+        public void CalculateH()
+        {
+            int input_row = rnd.Next(this.inputs.row);
+            if (this.hLayerCount == 0)
+            {
+
+                Matrix error, derivative , inp;
+
+                inp = this.inputs.getRow(input_row);
+                outputs = inp * this.weights[0];
+                outputs = ANN.sigmoid(outputs + bias[0]);
+
+
+                error = -(targets.getRow(input_row) - outputs);
+
+                derivative = sigmoidDerivative(outputs);
+
+                error = error.P2PProduct(derivative);
+
+
+                derivative = inp.GetTranspose() * error;
+
+                weights[0] = weights[0] - learningRate * derivative;
+
+                return;
+
+            }       
+
+            
+            Matrix c;
+            c = this.inputs.getRow(input_row) * this.weights[0];
+
+            this.hiddenLayers[0] = sigmoid(c + bias[0]) ;
+            for (int i = 1; i < this.hLayerCount; i++)
+            {
+                c = this.hiddenLayers[i - 1] * weights[i];
+                this.hiddenLayers[i] = sigmoid(c + bias[i]);
+            }
+            c = this.hiddenLayers[this.hLayerCount - 1] * weights[this.hLayerCount];
+            this.outputs = sigmoid(c + bias[this.hLayerCount]);
+            this.Backpropagate(input_row);
+
+        }
+
+        /// <summary>
+        ///  
+        /// </summary>
+        /// <param name="input_row">This parameter specifies example in the traning set </param>
+        public void Backpropagate(int input_row)
+        {
+            Matrix E = -(this.targets.getRow(input_row) - this.outputs).P2PProduct(sigmoidDerivative(outputs));
+            Matrix[] temp_weights = weights;
+            for (int i = 0; i < weights[this.hLayerCount].row; i++)
+            {
+                for (int j = 0; j < weights[this.hLayerCount].column; j++)
+                {
+                    temp_weights[this.hLayerCount].DataMatrix[i, j] = weights[this.hLayerCount].DataMatrix[i, j] - learningRate * hiddenLayers[this.hLayerCount - 1].DataMatrix[0,j]*E.DataMatrix[0,j]; 
+                }
+            }
+
+            for (int t = this.hLayerCount - 1; t > 0; t--)
+            {
+                E = E * weights[t];
+                E = E.P2PProduct(sigmoidDerivative(hiddenLayers[t]));
+                for (int i = 0; i < weights[t].row; i++)
+                {
+                    for (int j = 0; j < weights[t].column; j++)
+                    {
+                        temp_weights[t].DataMatrix[i, j] = weights[t].DataMatrix[i, j] -learningRate*hiddenLayers[t].DataMatrix[0, j] * E.DataMatrix[0, j];
+                    }
+                }
+
+            }
+
+            E = E*weights[1];
+            Matrix inp = inputs.getRow(input_row);
+            E = E.P2PProduct(inp);
+
+
+            for (int i = 0; i < weights[0].row; i++)
+            {
+                for (int j = 0; j < weights[0].column; j++)
+                {
+                    temp_weights[0].DataMatrix[i, j] = weights[0].DataMatrix[i, j] - learningRate * inp.DataMatrix[0,j]* E.DataMatrix[0, j];
+                }
+            }
+
+            weights = temp_weights;
+
+        }
+
+        public void PrintWeights()
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (i == 0)
+                {
+                    if(weights.Length == 1)
+                    {
+                        Console.WriteLine("Weights between input layer and output layer:");
+                    }
+                    else
+                        Console.WriteLine("Weights between input layer and Layer 0:");
+                }
+                else
+                {
+                    if(i == weights.Length -1)
+                        Console.WriteLine("Weights between Layer {0} and output layer:", i - 1, i + 1);
+                    else
+                        Console.WriteLine("Weights between Layer {0} and Layer {1}:", i - 1, i);
+                }
+                weights[i].PrintMatrix();
+            }
+        }
+
+        public static Matrix sigmoid(Matrix x)
+        {
+            Matrix C = new Matrix(x.row,x.column);
+            for (int i = 0; i < x.row; i++)
+            {
+                for (int j = 0; j < x.column; j++)
+                {
+                    C.DataMatrix[i, j] = (float)(1 / (1 + Math.Exp(-x.DataMatrix[i, j])));
+                }
+            }
+            return C;
+        }
+
+        public static Matrix sigmoidDerivative(Matrix x)
+        {
+            Matrix C = new Matrix(x.row,x.column);
+            for (int i = 0; i < x.row; i++)
+            {
+                for (int j = 0; j < x.column; j++)
+                {
+                    C.DataMatrix[i, j] = x.DataMatrix[i, j] * (1 - x.DataMatrix[i, j]);
+                }
+            }
+            return C;
+        }
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Matrix
+    {
+        public int row;
+        public int column;
+
+        public float[,] DataMatrix;
+        
+        public Matrix(float data)
+        {
+            this.row = 1;
+            this.column = 1;
+            this.DataMatrix = new float[1, 1];
+            DataMatrix[0, 0] = data;
+        }
+        public Matrix(float[,] data_matrix)
+        {
+            this.row = data_matrix.GetLength(0);
+            this.column = data_matrix.GetLength(1);
+            this.DataMatrix = data_matrix;
+        }
+        public Matrix(float[] data_matrix)
+        {
+            this.row = 1;
+            this.column = data_matrix.Length;
+            this.DataMatrix = new float[1,this.column];
+            for (int i = 0; i < this.column; i++)
+            {
+                this.DataMatrix[0,i] = data_matrix[i];
+            }
+            
+        }
+        public Matrix(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+            init();
+        }
+        private void init()
+        {
+            this.DataMatrix = new float[row,column];
+        }
+
+        public void Transpose()
+        {
+            float[,] temp = new float[column, row];
+            for (int i = 0; i < this.row; i++)
+            {
+                for (int j = 0; j < this.column; j++)
+                {
+                    temp[j, i] = DataMatrix[i, j];
+
+                }
+            }
+            int tmp = row;
+            row = column;
+            column = tmp;
+
+            DataMatrix = temp;
+        }
+        public Matrix GetTranspose()
+        {
+            Matrix temp = new Matrix(column, row);
+            for (int i = 0; i < this.row; i++)
+            {
+                for (int j = 0; j < this.column; j++)
+                {
+                    temp.DataMatrix[j, i] = DataMatrix[i, j];
+
+                }
+            }
+            return temp;
+        }
+
+        public Matrix getRow(int i)
+        {
+            float[] rw = new float[this.column];
+
+            for (int j = 0; j < this.column; j++)
+            {
+                rw[j] = this.DataMatrix[i, j];
+            }
+
+            return (new Matrix(rw));
+        }
+        public Matrix getColumn(int i)
+        {
+            float[] cl = new float[this.row];
+
+            for (int j = 0; j < this.row; j++)
+            {
+                cl[j] = this.DataMatrix[j, i];
+            }
+
+            return (new Matrix(cl).GetTranspose());
+        }
+        public void RandomFill()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < this.row; i++)
+            {
+                for (int j = 0; j < this.column; j++)
+                {
+                    DataMatrix[i, j] = (float)Math.Round(2*rnd.NextDouble()-1,5);
+                }
+            }
+        }
+
+        public void RandomFill(int upTo)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < this.row; i++)
+            {
+                for (int j = 0; j < this.column; j++)
+                {
+                    DataMatrix[i, j] = (float)rnd.Next(upTo);
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            Console.Write("[");
+            for (int i = 0; i < row; i++)
+            {
+                
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write(DataMatrix[i,j]);
+                    if (j != column - 1) Console.Write(",");
+                }
+                
+                if (i != row - 1) 
+                    Console.WriteLine(" ");
+                Console.Write(" ");
+            }
+            Console.WriteLine("]");
+            Console.WriteLine();
+        }
+
+        public static Matrix operator* (Matrix A , Matrix B)
+        {
+            if (B.row != A.column) throw new Exception("Matrix boyutları eşit değil.");
+
+            Matrix c = new Matrix(A.row,B.column);
+            float toplam = 0;
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < B.column; j++)
+                {
+                    toplam = 0;
+                    for (int t = 0; t < B.row; t++)
+                    {
+                        toplam += A.DataMatrix[i, t] * B.DataMatrix[t,j];
+                    }
+                    c.DataMatrix[i, j] = toplam;
+                }
+            }
+            return c;
+        }
+
+        public static Matrix operator* (Matrix A, int x)
+        {
+            
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] *= x;
+                }
+            }
+
+            return A;
+        }
+
+        public static Matrix operator *(float x ,Matrix A)
+        {
+
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] *= x;
+                }
+            }
+
+            return A;
+        }
+
+        public static Matrix operator *(Matrix A, float x)
+        {
+
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] *= x;
+                }
+            }
+
+            return A;
+        }
+        public static Matrix operator +(float x, Matrix A)
+        {
+
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] += x;
+                }
+            }
+
+            return A;
+        }
+        public static Matrix operator +(Matrix A ,float x)
+        {
+
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] += x;
+                }
+            }
+
+            return A;
+        }
+
+        public static Matrix operator *(int x, Matrix A)
+        {
+
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    A.DataMatrix[i, j] *= x;
+                }
+            }
+
+            return A;
+        }
+
+        public static Matrix operator+ (Matrix A, Matrix B)
+        {
+            if (A.row != B.row || A.column != B.column) throw new Exception("Matrix boyutları eşit değil.");
+            Matrix C = new Matrix(A.row,A.column);
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    C.DataMatrix[i,j] = A.DataMatrix[i, j] + B.DataMatrix[i, j];
+                }
+            }
+
+            return C;
+        }
+
+        public static Matrix operator- (Matrix A, Matrix B)
+        {
+            if ((A.row != B.row) || (A.column != B.column)) throw new Exception("Matrix boyutları eşit değil.");
+            Matrix C = new Matrix(A.row,A.column);
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    C.DataMatrix[i,j] = A.DataMatrix[i, j] - B.DataMatrix[i, j];
+                }
+            }
+
+            return C;
+        }
+
+        public static Matrix operator -(Matrix A)
+        {
+            Matrix C = new Matrix(A.row, A.column);
+            for (int i = 0; i < A.row; i++)
+            {
+                for (int j = 0; j < A.column; j++)
+                {
+                    C.DataMatrix[i, j] = -A.DataMatrix[i, j];
+                }
+            }
+
+            return C;
+        }
+
+
+        // point to point product
+        public Matrix P2PProduct(Matrix B)
+        {
+            if ((this.row != B.row) || (this.column != B.column)) throw new Exception("Row or column count not equal.");
+            Matrix C = new Matrix(B.row, B.column);
+            for (int i = 0; i < this.row; i++)
+            {
+                for (int j = 0; j < this.column; j++)
+                {
+                    C.DataMatrix[i, j] = this.DataMatrix[i, j] * B.DataMatrix[i, j];
+                }
+            }
+
+            return C;
+        }
+    }
+}
